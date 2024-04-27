22a31a2aa3ac065af907e18f4497a38b6390e757
Abu Bakar <abubakarzafar2029@gmail.com>
2024-04-27 09:28:32 +0500

    Arrays and 2D arrays completed

---
+++ Arrays And ArrayList/Arrays.java
@@ -0,0 +1,10 @@
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        int[] rnos = new int[5];
+        System.out.println(rnos[2]); // by default all values will be 0
+
+        // in String it will be null
+    }
+}
---
+++ Arrays And ArrayList/MultiDimension.java
@@ -0,0 +1,15 @@
+
+public class MultiDimension {
+    public static void main(String[] args) {
+        /*
+         * 1 2 3
+         * 4 2 5
+         * 8 9 0
+         * 5 9 9
+         */
+
+        int[][] arr = new int[4][]; // coloumns are not mendatory
+        int[][] arr2D = { { 2, 3 }, { 4, 5, 6 }, { 9 }, { 2, 3, 4 } };
+        System.out.println(arr2D.length); // no. of rows
+    }
+}
