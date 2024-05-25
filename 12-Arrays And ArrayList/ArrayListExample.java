876123e573681755ed2a4a14351c698568ac5331
Abu Bakar <abubakarzafar2029@gmail.com>
2024-05-25 23:05:59 +0500

    Learned 2D arrays and arrayList

---
+++ 12-Arrays And ArrayList/ArrayListExample.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// when array size is undefined 
+public class ArrayListExample {
+    public static void main(String[] args) {
+        // Syntax
+        ArrayList<Integer> list = new ArrayList<>(10);
+    }
+}
