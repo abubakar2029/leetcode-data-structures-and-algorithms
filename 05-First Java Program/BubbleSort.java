80b5ef10d26f1c0ed0eb28fb3350d6ba8ef93a12
Abu Bakar <abubakarzafar2029@gmail.com>
2024-05-30 17:23:48 +0500

    Bubble sort code modified into reusable methods

--- 05-First Java Program/BubbleSort.java
+++ 05-First Java Program/BubbleSort.java
@@ -27,25 +27,35 @@ public class BubbleSort {
         System.out.println(Arrays.toString(arr));
     }
 }*/
+
 import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {5, 3, 4,-99,0,-7, 1, 2};
+        int[] arr = {1, 33, 4, -99, 0, -7, 1, 2, 5};
+        swap(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void swap(int[] arr){
+        boolean swap;
         // run the steps n-1 times
         for (int i = 1; i < arr.length; i++) {
+            swap = false;
             // for each step, max item will come at the last respective index
             for (int j = 0; j < arr.length - i; j++) {
                 // swap if the item is smaller than the previous item
-                if (arr[j] > arr[j +1]) {
-                    // swap
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr,j,j+1);
+                    swap = true;
                 }
             }
-
+            if (!swap)
+                break;
         }
-        System.out.println(Arrays.toString(arr));
+    }
+    static void swap(int[] arr,int start,int end){
+        int temp=arr[end];
+        arr[end]=arr[start];
+        arr[start]=temp;
     }
 }
