b96b548c1f863099c90085fc9ae2da988b026415
Abu Bakar <abubakarzafar2029@gmail.com>
2024-06-20 21:37:59 +0500

    Completed palindrome in recursion and learnt bitwise opeartors

--- Resursion20/CheckPalindrome_With_Recursion.java
+++ Resursion20/CheckPalindrome_With_Recursion.java
@@ -1,16 +1,16 @@
 public class CheckPalindrome_With_Recursion {
     public static void main(String[] args) {
-        System.out.println(palindrome(070));
+        System.out.println(palindrome(88988));
     }
 
-    static int reverseHelper(int integer, int reversed) {
-        if (integer == 0) return reversed;
+    static int reverseHelper(int integer) {
         int remainder = integer % 10;
-        reversed = reversed * 10 + remainder;
-        return reverseHelper(integer / 10, reversed);
+        integer = integer / 10;
+        if (integer == 0) return remainder;
+        return ((reverseHelper(integer) * 10) + remainder);
     }
 
     static boolean palindrome(int n) {
-        return reverseHelper(n, 0)==n;
+        return reverseHelper(n) == n;
     }
 }
