8c9607c4c64a6f2fadadb0f8f316ed6b95e76797
Muhammad Abu Bakar <abubakarzafar2029@gmail.com>
2025-01-08 23:30:50 +0500

    Day 22 - Leetcode 66 solved

--- src/_66_Plus_One.java
+++ src/_66_Plus_One.java
@@ -1,27 +1,23 @@
 public class _66_Plus_One {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] result = s.plusOne(new int[]{9,8,7,6,5,4,3,2,1,0});
+        int[] result = s.plusOne(new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0});
     }
 
     static class Solution {
         public int[] plusOne(int[] digits) {
-            // converting integer array into string
-            String str = "";
-            for (int i = 0; i < digits.length; i++) {
-                str += digits[i];
+
+//            iterating over the loop
+            for (int i = digits.length - 1; i >= 0; i--) {
+                if (digits[i] < 9) {
+                    digits[i]++;
+                    return digits;
+                }
+                digits[i] = 0;
             }
-            // converting str to integer and adding 1
-            int largeInteger = Integer.parseInt(str);
-            largeInteger++;
-            str = "";
-            str = "" + largeInteger;
-            int[] new_digits = new int[str.length()];
-            // again converting into int[]
-            for (int i = 0; i < new_digits.length; i++) {
-                new_digits[i] = Character.getNumericValue(str.charAt(i));
-            }
-            return new_digits;
+            digits = new int[digits.length + 1];
+            digits[0] = 1;
+            return digits;
         }
     }
 }
