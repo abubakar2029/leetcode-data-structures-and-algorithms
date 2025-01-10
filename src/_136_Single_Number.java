bdf1c639b09982fbbee2ebf7fb1f75826ffdc609
Muhammad Abu Bakar <abubakarzafar2029@gmail.com>
2025-01-10 22:47:24 +0500

    Day 24 - Solution 2 failed of leetcode 163

--- src/_136_Single_Number.java
+++ src/_136_Single_Number.java
@@ -3,7 +3,11 @@ import java.util.Map;
 
 
 public class _136_Single_Number {
-    class Solution {
+    public static void main(String[] args) {
+        int result = Solution2.singleNumber(new int[]{4, 1, 2, 1, 2});
+    }
+
+    class Solution1 {
         public int singleNumber(int[] nums) {
             Map<Integer, Integer> record = new HashMap<>();
 
@@ -20,6 +24,19 @@ public class _136_Single_Number {
             }
 
             return -1; // Default return if no single number is found (shouldn't happen with valid input)
+        }
+    }
+
+    class Solution2 {
+        public static int singleNumber(int[] nums) {
+            int pointer = 0;
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[pointer] != nums[i]) continue;
+                else {
+                    pointer++;
+                }
+            }
+            return pointer;
         }
     }
 }
