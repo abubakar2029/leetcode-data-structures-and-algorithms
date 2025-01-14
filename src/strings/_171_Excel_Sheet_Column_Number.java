3c3cd6ab9d5e2f8fe7a65cc7ff8b7ee1fe5f6e8c
Muhammad Abu Bakar <abubakarzafar2029@gmail.com>
2025-01-14 23:52:41 +0500

    Day 28 - Solved Leetcode 171

--- src/strings/_171_Excel_Sheet_Column_Number.java
+++ src/strings/_171_Excel_Sheet_Column_Number.java
@@ -3,20 +3,32 @@ package strings;
 public class _171_Excel_Sheet_Column_Number {
     public static void main(String[] args) {
 
-        int a = Solution.titleToNumber("FXSHRXW");
+        int a = Solution.titleToNumber("A");
 //        int a = Solution.titleToNumber("ZY");
         System.out.println(a);
     }
 
-    class Solution {
+    class SolutionDay1 {
         static public int titleToNumber(String columnTitle) {
             int coloumnNumber = 0;
             for (int i = 0; i < columnTitle.length() - 1; i++) {
                 int baseNumber = columnTitle.charAt(i) - 64;
                 coloumnNumber += 26 * baseNumber;
             }
-            coloumnNumber += columnTitle.charAt(columnTitle.length() - 1)-64;
+            coloumnNumber += columnTitle.charAt(columnTitle.length() - 1) - 64;
             return coloumnNumber;
         }
+    }
+
+    class Solution {
+        static public int titleToNumber(String columnTitle) {
+            int cN = 0; // coloumnNumber
+            for (int i = 0; i <= columnTitle.length() - 1; i++) {
+                int baseNumber = columnTitle.charAt(i) - 64;
+                cN += Math.pow(26, columnTitle.length() - 1 - i) * baseNumber;
+            }
+            return cN;
+        }
+
     }
 }
