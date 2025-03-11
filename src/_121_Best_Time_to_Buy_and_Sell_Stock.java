bee7f0722a7da260efe896365a597afdff6b2239
Muhammad Abu Bakar <abubakarzafar2029@gmail.com>
2025-03-11 22:56:51 +0500

    Day 11 - Leetcode 121 solved in 3rd attempt

--- src/_121_Best_Time_to_Buy_and_Sell_Stock.java
+++ src/_121_Best_Time_to_Buy_and_Sell_Stock.java
@@ -1,10 +1,28 @@
 public class _121_Best_Time_to_Buy_and_Sell_Stock {
 
     public static void main(String[] args) {
-        System.out.println(Solution.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+//        System.out.println(Solution1.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
     }
 
-    class Solution {
+    //    third attempt passed all test cases
+    class Solution3 {
+        public int maxProfit(int[] prices) {
+            int minCurrent = 0;
+            int maxProfit = 0;
+
+            for (int i = 0; i < prices.length; i++) {
+                if (prices[i] < prices[minCurrent]) {
+                    minCurrent = i;
+                } else if (maxProfit < prices[i] - prices[minCurrent]) {
+                    maxProfit = prices[i] - prices[minCurrent];
+                }
+            }
+            return maxProfit;
+        }
+    }
+
+    //    first attempt didn't work
+    class Solution1 {
         static public int maxProfit(int[] prices) {
             int maxProfit = 0;
             int i = 0;
@@ -20,6 +38,29 @@ public class _121_Best_Time_to_Buy_and_Sell_Stock {
                 i++;
             }
             return maxProfit;
+        }
+    }
+
+    //    second attempt didn't work
+    class Solution2 {
+        public int maxProfit(int[] prices) {
+            int n = prices.length;
+            int minIndex = 0;
+            int maxIndex = 0;
+
+            // pehla loop maximum stock find krna ka lia
+            for (int i = 0; i < n; i++) {
+                if (prices[i] > prices[maxIndex]) {
+                    maxIndex = i;
+                }
+            }
+            // second loop maximum sa pehla minimum find krna ka lia
+            for (int i = 1; i <= maxIndex; i++) {
+                if (prices[i] < prices[minIndex]) {
+                    minIndex = i;
+                }
+            }
+            return (prices[maxIndex] - prices[minIndex]);
         }
     }
 }
