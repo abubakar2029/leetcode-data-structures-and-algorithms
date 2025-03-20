592e4a8576bbfe8b495f2402557842b6c954c9f7
Muhammad Abu Bakar <abubakarzafar2029@gmail.com>
2025-03-20 10:31:46 +0500

    Day 17 - Leetcode 713 solved using dynamic window sliding approach

--- src/slidingWindow/_713_Subarray_Product_Less_Than_K.java
+++ src/slidingWindow/_713_Subarray_Product_Less_Than_K.java
@@ -2,15 +2,16 @@ package slidingWindow;
 
 public class _713_Subarray_Product_Less_Than_K {
     public static void main(String[] args) {
-        System.out.println(Solution1.numSubarrayProductLessThanK(new int[]{10, 9, 10, 4, 3, 8, 3, 3, 6, 2, 10, 10, 9, 3}, 19));
+//        System.out.println(Solution1.numSubarrayProductLessThanK(new int[]{20,
+//                10, 9, 10, 4, 3, 8, 3, 3, 6, 2, 10, 10, 9, 3}, 19));
+        System.out.println(Solution2.numSubarrayProductLessThanK(new int[]{1, 2, 3}, 0));
     }
 
 
-//    Time Limit Exceeded
+    //    Time Limit Exceeded
     static class Solution1 {
         public static int numSubarrayProductLessThanK(int[] nums, int k) {
             int count = 0;
-
             for (int i = 0; i < nums.length; i++) {
                 int product = 1;
                 for (int j = i; j < nums.length; j++) {
@@ -21,6 +22,24 @@ public class _713_Subarray_Product_Less_Than_K {
                     System.out.println(product);
                     count++;
                 }
+            }
+            return count;
+        }
+    }
+
+    //    sliding window
+    static class Solution2 {
+        static public int numSubarrayProductLessThanK(int[] nums, int k) {
+            int count = 0;
+            int product = 1;
+            int start = 0;
+            for (int end = 0; end < nums.length; end++) {
+                product *= nums[end];
+                while (product >= k) {
+                    product /= nums[start];
+                    start++;
+                }
+                count += end - start + 1;
             }
             return count;
         }
