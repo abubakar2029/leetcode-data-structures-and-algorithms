a257eeb082e2c54854eda155f44f6c1f90cfb792
Muhammad Abu Bakar <abubakarzafar2029@gmail.com>
2025-03-26 16:48:45 +0500

    Day 23 - solved leetcode 441

---
+++ src/_441_Arranging_Coins.java
@@ -0,0 +1,33 @@
+public class _441_Arranging_Coins {
+    public static void main(String[] args) {
+        Solution.arrangeCoins(6);
+    }
+
+    static class Solution {
+        static public int arrangeCoins(int n) {
+            int i = 1; // which row we are on
+            while (n > 0) { // checking to see if we have used all our coins
+                n = n - i; // adding coins to our row
+                i++; // increasing our row
+            }
+            return i - 1; // we return our current row minus one because the last row is our completed row
+        }
+    }
+
+    //    Time limit exceeded - Naive approach
+    class Solution1 {
+        public int arrangeCoins(int n) {
+            int elements = n;
+            for (int rows = 1; rows <= n; rows++) {
+                elements -= rows;
+                System.out.println(elements);
+                if (elements == 0) {
+                    return rows;
+                } else if (elements < 0) {
+                    return rows - 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
