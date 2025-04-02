eba29b2415dfd92c36075dfa4219c42575cbcf73
Muhammad Abu Bakar <abubakarzafar2029@gmail.com>
2025-04-02 21:32:33 +0500

    Day 30 - leetcode 1652 solved with approach 2

--- src/Temp.java
+++ src/Temp.java
@@ -1,6 +1,8 @@
-    import java.util.*;
-    public class Temp {
-        public static void main(String[] args) {
-            System.out.println(7%2);
-        }
+import java.util.*;
+
+public class Temp {
+    public static void main(String[] args) {
+
+        System.out.println();
     }
+}
--- src/slidingWindow/_1652_Defuse_the_Bomb.java
+++ src/slidingWindow/_1652_Defuse_the_Bomb.java
@@ -2,10 +2,49 @@ package slidingWindow;
 
 public class _1652_Defuse_the_Bomb {
     public static void main(String[] args) {
-
+        Solution2.decrypt(new int[]{2, 4, 9, 3}, -2);
     }
 
-    class Solution {
+
+    static class Solution2 {
+        static public int[] decrypt(int[] code, int k) {
+            int n = code.length;
+            int[] decrypt = new int[n];
+            if (k > 0) {
+                // initialize the window
+                int sum = 0;
+                for (int i = 0; i < k; i++) {
+                    sum += code[i];
+                }
+
+                for (int i = 0; i < n; i++) {
+                    sum = sum - code[i] + code[(i + k) % n];
+                    decrypt[i] = sum;
+                }
+            } else if (k == 0) {
+                return decrypt;
+            } else {
+                k = -k;
+
+                // initialize the window
+                int sum = 0;
+                for (int i = 0; i < k; i++) {
+                    sum += code[n - i - 1];
+                }
+
+                // slide the window
+                for (int i = n - 1; i >= 0; i--) {
+                    sum = sum - code[i] + code[(i - k+n) % n];
+                    decrypt[i] = sum;
+                }
+
+            }
+            return decrypt;
+        }
+    }
+
+    //    incorrect
+    class Solution1 {
         public int[] decrypt(int[] code, int k) {
             int n = code.length;
             int[] decrypt = new int[n];
