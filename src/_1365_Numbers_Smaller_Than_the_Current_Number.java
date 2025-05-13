d4fe9bfce26145762f9be6c9009bdbc0674910db
Muhammad Abu Bakar <abubakarzafar2029@gmail.com>
2025-05-13 21:01:33 +0500

    Day 56 - solved leetcode 1365

--- src/_1365_Numbers_Smaller_Than_the_Current_Number.java
+++ src/_1365_Numbers_Smaller_Than_the_Current_Number.java
@@ -1,31 +1,58 @@
+import java.util.*;
+
 public class _1365_Numbers_Smaller_Than_the_Current_Number {
     public static void main(String[] args) {
         _1365_Numbers_Smaller_Than_the_Current_Number outer = new _1365_Numbers_Smaller_Than_the_Current_Number();
         Solution sl = outer.new Solution();
-        sl.smallerNumbersThanCurrent(new int[] {8,1,2,2,3});
+        sl.smallerNumbersThanCurrent(new int[]{8, 1, 2, 2, 3});
     }
 
+    // Counting sort approach
     class Solution {
         public int[] smallerNumbersThanCurrent(int[] nums) {
+            int n = nums.length;
+            int[] result = new int[n];
+
+            // making the frequency array
             int[] count = new int[101];
-            int[] res = new int[nums.length];
+            for (int i = 0; i < n; i++) {
+                int digit = nums[i];
+                count[digit] += 1;
+            }
+
+            // calculating the prefix count
+            for (int i = 1; i < 100; i++) {
+                count[i] = count[i - 1] + count[i];
+            }
+
+            // making the resultant array
+            for (int i = 0; i < n; i++) {
+                result[i] = nums[i] == 0 ? 0 : count[nums[i] - 1];
+            }
+
+            return result;
+        }
+    }
+
+    //    Hashmap approach
+    class Solution2 {
+        public int[] smallerNumbersThanCurrent(int[] nums) {
+
+            int[] result = nums.clone();
+            Arrays.sort(result);
+
+            Map<Integer, Integer> table = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                if (!table.containsKey(result[i])) {
+                    table.put(result[i], i);
+                }
+            }
 
             for (int i = 0; i < nums.length; i++) {
-                count[nums[i]]++;
+                result[i] = table.get(nums[i]);
             }
 
-            for (int i = 1; i <= 100; i++) {
-                count[i] += count[i - 1];
-            }
-
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == 0)
-                    res[i] = 0;
-                else
-                    res[i] = count[nums[i] - 1];
-            }
-
-            return res;
+            return result;
         }
     }
 }
