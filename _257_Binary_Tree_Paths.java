d2cf55eaccfdb09b171cbe3101162dcb93e2dddb
Muhammad Abu Bakar <abubakarzafar2029@gmail.com>
2025-08-17 23:17:40 +0500

    Day 82 - Solved leetcode 257

---
+++ _257_Binary_Tree_Paths.java
@@ -0,0 +1,27 @@
+public class _257_Binary_Tree_Paths {
+
+    class Solution {
+        List<String> ans = new ArrayList();
+
+        public List<String> binaryTreePaths(TreeNode root) {
+            helper(root, "");
+            return ans;
+        }
+
+        public void helper(TreeNode node, String str) {
+            if (node == null)
+                return;
+
+            // It is leaf node, so add it and return
+            if (node.left == null && node.right == null) {
+                str = str + node.val;
+                ans.add(str);
+                return;
+            }
+            str = str + node.val + "->";
+            helper(node.left, str);
+            helper(node.right, str);
+
+        }
+    }
+}
