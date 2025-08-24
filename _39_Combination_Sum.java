939e0ec6c0b74137c503b51de880a1d7bd2c7f37
Muhammad Abu Bakar <abubakarzafar2029@gmail.com>
2025-08-24 09:30:26 +0500

    Day 88 - solved combination sum with recursion approach

--- _39_Combination_Sum.java
+++ _39_Combination_Sum.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class _39_Combination_Sum {
 
+    public static void main(String[] args) {
+        Solution2.combinationSum(new int[]{2, 3, 6, 7}, 7);
+    }
 
 //    My first intuition
     // outer_loop
@@ -15,7 +18,42 @@ public class _39_Combination_Sum {
     // if sum == target
     // -> add it's subset and continue
     //
-    class Solution {
+
+    static class Solution2 {
+        static  Set<List<Integer>> set = new HashSet();
+
+        static public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+            allCombinations(candidates, 0, target, new ArrayList<Integer>());
+            return new ArrayList<List<Integer>>(set);
+        }
+
+        static public void allCombinations(int[] arr, int start, int target, ArrayList<Integer> current) {
+            // I check if target is completed
+            if (target == 0) {
+                // if (set.contains(current)) {
+                set.add(new ArrayList<>(current));
+                // }
+                return;
+            }
+
+            // I check if it goes smaller than target
+            if (target < 0 || start >= arr.length)
+                return;
+
+            // I include the element
+            current.add(arr[start]);
+            // sigle included call
+            allCombinations(arr, start + 1, target - arr[start], current);
+            // same element call
+            allCombinations(arr, start, target - arr[start], current);
+            current.remove(current.size() - 1);
+            // excluding the element
+            allCombinations(arr, start + 1, target , current);
+        }
+    }
+
+    class Solution1 {
         public List<List<Integer>> combinationSum(int[] candidates, int target) {
             List<List<Integer>> result = new ArrayList<>();
             backtrack(candidates, target, 0, new ArrayList<>(), result);
