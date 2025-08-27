f2b31b15170d1d43bd2acb11c9a98acee70f6707
Muhammad Abu Bakar <abubakarzafar2029@gmail.com>
2025-08-27 20:44:53 +0500

    Day 90 - Combination sum II, backtracking passed 1 test case but wrong answer on submission

---
+++ backtracking/_40_Combination_Sum_II.py
@@ -0,0 +1,24 @@
+class Solution:
+    my_set = set()
+
+    def combinationSum2(self, candidates, target: int):
+        candidates.sort()
+        self.helper(0, target, [], candidates)
+        return list(self.my_set)
+
+    def helper(self, index, target, current, arr):
+        if target == 0:
+            self.my_set.add(tuple(current))
+            return
+
+        if target < 0 or index >= len(arr):
+            return
+
+        current.append(arr[index])
+        self.helper(index + 1, target - arr[index], current, arr)
+        current.pop()
+        self.helper(index + 1, target, current, arr)
+
+a = Solution()
+ab=a.combinationSum2([2,5,2,1,2],5)
+print(ab)
